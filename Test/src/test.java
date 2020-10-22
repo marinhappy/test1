61cc2145addeb5bf92179899258b37df8a44dc6e
User <User@DESKTOP-0S535PU>
2020-10-22 21:37:11 +0800

    8

--- Test/src/test.java
+++ Test/src/test.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub2
 		ArrayList<String> list=new ArrayList<String>();
 		list.add("marin");
 		list.add("love");
